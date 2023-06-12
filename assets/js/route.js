b7ebe8fa2f23390355f246f2505f145c81f09c3c
NaufalFiqri <naufal11bale@gmail.com>
2023-06-13 02:54:28 +0800

    init routejs

--- assets/js/route.js
+++ assets/js/route.js
@@ -3,3 +3,16 @@
  * @copyright naufalfiqri 2023 All rights reserved
  * @author  naufalfiqri <naufalfiqrifauzi@gmail.com>
  */
+
+"use strict";
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"; // London
+
+const currentLocation = function () {};
+
+const searchedLocation = (query) => {};
+
+const routes = new Map([
+  ["/current-location", currentLocation],
+  ["/weather", searchedLocation],
+]);

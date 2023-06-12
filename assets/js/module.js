8993a81ce91b8b7e18ace50dde622e82a620cfc7
NaufalFiqri <naufal11bale@gmail.com>
2023-06-12 16:37:24 +0800

    init module.js

--- assets/js/module.js
+++ assets/js/module.js
@@ -3,3 +3,45 @@
  * @copyright naufalfiqri 2023 All rights reserved
  * @author  naufalfiqri <naufalfiqrifauzi@gmail.com>
  */
+
+"use strict";
+
+export const weekDayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+/**
+ * @param {number} dateUnix Unix date in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Date string, formate, "Sunday 10, Jan"
+ */
+
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekDayName = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+
+  return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+};
+

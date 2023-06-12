73960ade75729e5124abd217bb366940f50159a2
NaufalFiqri <naufal11bale@gmail.com>
2023-06-13 03:15:04 +0800

    init appjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,18 @@
  * @copyright naufalfiqri 2023 All rights reserved
  * @author  naufalfiqri <naufalfiqrifauzi@gmail.com>
  */
+
+"use strict";
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
+/**
+ * Add event listener on multiple elements
+ * @param {NodeList} elements Elements node array
+ * @param {*} eventType 
+ * @param {*} callback 
+ */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elemtnt of elements) element.addEventListener(eventType, callback);
+};
